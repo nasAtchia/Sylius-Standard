c30bc193af1512c8b350ec5f32f5e2aea8c2589a
Annas Atchia <nasatchia@gmail.com>
2024-11-09 17:51:50 +0400

    Refactor show.js script

--- assets/shop/scripts/product/show.js
+++ assets/shop/scripts/product/show.js
@@ -1,7 +1,4 @@
-/**
- * Displays a "Great Choice!" message when the user selects a quantity of 70.
- */
-function displayGreatChoiceMessage() {
+(function () {
   const quantityInput = document.querySelector('[name="sylius_shop_add_to_cart[cartItem][quantity]"]');
 
   if (!quantityInput) return;
@@ -11,12 +8,9 @@ function displayGreatChoiceMessage() {
 
     if (isNaN(value)) return;
 
+    // Displays a "Great Choice!" message when the user selects a quantity of 70.
     if (value === 70) {
       alert('Great Choice!');
     }
   });
-}
-
-(function () {
-  displayGreatChoiceMessage();
 })();
